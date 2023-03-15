d7a7b1ef4293cd5e67adb9653adc6cc6b41c76f4
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-03-15 19:22:48 +0200

    created the edit function in views

--- students/forms.py
+++ students/forms.py
@@ -20,6 +20,6 @@ class StudentForm(forms.ModelForm):
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'field_of_study': forms.Select(attrs={'class': 'form-control'}),
+            'field_of_study': forms.TextInput(attrs={'class': 'form-control'}),
             'gpa': forms.NumberInput(attrs={'class': 'form-control'}),
         }
